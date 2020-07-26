24902e863ae20a2e9b3b9d80656ab65d48a469f0
aawillats <adamwillats@gmail.com>
2020-07-26 17:06:22 -0400

    comments cleaned up

--- generateStimulusOutline_StAC_mid2020.m
+++ generateStimulusOutline_StAC_mid2020.m
@@ -14,9 +14,6 @@ sec2samples = @(t_ms) floor((t_ms/dt));
 preTimeDefault = 0.5;
 postTimeDefault = 0.5;
 
-%bufferSegmentTimes(preTime, segement, postTime);
-%defualtBufferSegment = @(segment) bufferSegmentTimes(preTimeDefault, segment, postTimeDefault);
-
 
 %% Channel definitions - pulled from 2020.01.15.MBm04_awake3
 
@@ -46,8 +43,6 @@ fOFF = @(len,params) zeros(1,len);
 
 wClip = [-5,5]
 clipFun = @(x,xmin,xmax) min(max(x,xmin),xmax);
-
-
 
 
 optoNoiseMag = 3;
@@ -127,40 +122,17 @@ Phase1A.times = [0.5, 5.0, 4. ];
 Phase1A.nreps = 10;
 Phase1A.dt = dt;
 
-% end of user-entered options, code below is intended to be functionalized
-% to work for lots of scenarios
-%[ Phase1A.conditions, Phase1A.nCond ] = combineConditionsMulti(Phase1A.options)
 Phase1A.conditions = sweepConditions;
 Phase1A.nCond = length(sweepConditions);
-%[ Phase1A ] = generateStimuli( Phase1A, ConditionMap );
 [ Phase1A ] = generateStimuli( Phase1A, ConditionStructMap ,ChannelMap,ColorsMap);
-
-
 
 figure(2)
 clf
-
 plotExptPhase(Phase1A)
-%{
-subplot(2,2,1)
-hold on
-title([Phase1A.title,'[single trial]'] )
-for i = 1:Phase1A.nCond
-    plot(Phase1A.t{i}, Phase1A.segments{i})
-end
-
-subplot(2,1,2)
-plot(Phase1A.fullt, Phase1A.fullSegment,'LineWidth',1,'Color','r')
-title('[all trials]')
-%}
 
 set(gcf,'Position',[144   563   676   242])
 
 %% Phase 1B: Identification stimulus
-
-%how params are being used here is different than above 
-
-
 Phase1B.title = 'Phase 1B: Whisker & Opto Identification Stimulus';
 
 whisk_cond = buildConditionCell({'whisk_OFF','whisk_ON'});
@@ -171,48 +143,10 @@ Phase1B.times = [0.5, 5.0, 0.5];
 Phase1B.nreps = 10;
 Phase1B.dt = dt;
 
-% end of user-entered options, code below is intended to be functionalized
-% to work for lots of scenarios
 [ Phase1B.conditions, Phase1B.nCond ] = combineConditionsMulti(Phase1B.options)
 [ Phase1B ] = generateStimuli( Phase1B, ConditionStructMap ,ChannelMap,ColorsMap);
-
-
 
 figure(3)
 clf
 plotExptPhase(Phase1B)
 set(gcf,'Position',[   144   171   676   318])
-
-%{
-hold on
-for i = 1:Phase1B.nCond
-    seg = Phase1B.segments{i};
-    for j = 1:size(seg,1)
-        plot(Phase1B.t{i},seg(j,:)+Phase1B.channelIDs{i}(j), 'Color',Phase1B.plotColors{i}{j})
-    end
-end
-title([Phase1B.title,'[single trial]'] )
-
-
-subplot(2,1,2)
-hold on
-for j = 1:size(Phase1B.fullSegment,1)
-    plot(Phase1B.fullt, Phase1B.fullSegment(j,:)+Phase1B.channelIDs{1}(j), 'Color',Phase1B.plotColors{1}{j})
-end
-title([Phase1B.title,'[all trials]'] )
-%}
-
-
-%{
-
-
-% {{'whisk',true},{'whisk',false} x {{'opto_noise',false},{'opto_noise',true}}
-[WO,nWO] = combineConditions(whisk_cond, opto_noise_cond);
-
-[conditions_phase1, nCond_phase1] = combineConditionsMulti({whisk_cond, opto_noise_cond})
-
-
-TrialLength_min_phase1 = nCond_phase1 * reps_phase1 * sum(times_phase1)/60;
-
-sprintf('Phase 1 is projected to take %.1f minutes, with %i trials of %i conditions',TrialLength_min_phase1, reps_phase1, nCond_phase1)
-%}
