7d2d4ec8694b9f7176d9f3588909e18b5c9a3f2c
kjohnsen <kjohnsen6@gatech.edu>
2021-07-16 15:23:44 -0400

    Install deps with vcpkg

--- cmake/Modules/HDF5.cmake
+++ cmake/Modules/HDF5.cmake
@@ -1,25 +1,2 @@
-find_package(HDF5 COMPONENTS C)
-
-if(HDF5_FOUND)
-  list(APPEND PROJECT_REQUIRED_LIBRARIES_ABSOLUTE_NAME ${HDF5_LIBRARIES})
-else()
-  # try pkg-config
-  if (PKG_CONFIG_FOUND)
-    pkg_check_modules(HDF5 hdf5)
-
-    if (HDF5_FOUND)
-      list(APPEND PROJECT_REQUIRED_LIBRARY_DIRS ${HDF5_LIBRARY_DIRS})
-      list(APPEND PROJECT_REQUIRED_LIBRARIES_SHORT_NAME ${HDF5_LIBRARIES})
-    endif()
-
-  endif()
-endif()
-
-if(HDF5_FOUND)
-  list(APPEND PROJECT_REQUIRED_INCLUDE_DIRS ${HDF5_INCLUDE_DIRS})
-  if (WIN32)
-    set(PROJECT_REQUIRED_CXX_FLAGS "${PROJECT_REQUIRED_CXX_FLAGS} -DH5_BUILT_AS_DYNAMIC_LIB")
-  endif()
-else()
-  message(FATAL_ERROR "HDF5 was not found by cmake or pkg-config.")
-endif()
+find_package(hdf5 CONFIG REQUIRED)
+list(APPEND PROJECT_REQUIRED_LIBRARIES_ABSOLUTE_NAME hdf5::hdf5-shared hdf5::hdf5_hl-shared)
---
+++ cmake/Modules/Python3.cmake
@@ -0,0 +1,2 @@
+find_package(Python3 COMPONENTS Development)
+# list(APPEND PROJECT_REQUIRED_LIBRARIES_ABSOLUTE_NAME Python3::Python)
