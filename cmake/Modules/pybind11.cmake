7ba11a4480317060e4864cb6c2a4a043cb0480aa
kjohnsen <kjohnsen6@gatech.edu>
2021-08-12 10:36:51 -0400

    Update CMake for carma

--- cmake/Modules/pybind11.cmake
+++ cmake/Modules/pybind11.cmake
@@ -1,4 +1,8 @@
+# these are needed for CARMA
+find_package(Python3 COMPONENTS Interpreter Development NumPy)
 add_subdirectory(python/pybind11)
-if (Python_FOUND OR PYTHON_EXECUTABLE)
+add_subdirectory(python/carma)
+# in case you pass in PYTHON_EXECUTABLE directly
+if (Python3_FOUND OR PYTHON_EXECUTABLE)
     set(Python_FOUND TRUE)
 endif()
