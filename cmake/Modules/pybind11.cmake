6fa3dd658d904b1481de41986c7890699e5456d1
kjohnsen <kjohnsen6@gatech.edu>
2021-10-05 11:15:03 -0400

    Add install option

--- cmake/Modules/pybind11.cmake
+++ cmake/Modules/pybind11.cmake
@@ -1,8 +1,17 @@
 # these are needed for CARMA
 find_package(Python3 COMPONENTS Interpreter Development NumPy)
 add_subdirectory(python/pybind11)
-add_subdirectory(python/carma)
+# to make compatible for install:
+if(LDSCTRLEST_INSTALL)
+  # need to use installed version of carma which has export set defined
+  FIND_PACKAGE(carma CONFIG REQUIRED)
+else()
+  add_subdirectory(python/carma)
+endif()
+# carma must be linked to ldsCtrlEst, not just binding modules
+# since carma needs to be able to change how Armadillo manages memory
+list(APPEND PROJECT_REQUIRED_LIBRARIES_ABSOLUTE_NAME carma::carma)
 # in case you pass in PYTHON_EXECUTABLE directly
 if (Python3_FOUND OR PYTHON_EXECUTABLE)
-    set(Python_FOUND TRUE)
+  set(Python_FOUND TRUE)
 endif()
