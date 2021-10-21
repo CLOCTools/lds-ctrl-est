0a6cf18d692a3aa871fffd1da603d905f138f1dd
kjohnsen <kjohnsen6@gatech.edu>
2021-10-21 13:36:18 -0400

    Finish eg_glds_fit.py

--- python/tests/test_examples.py
+++ python/tests/test_examples.py
@@ -9,4 +9,7 @@ examples = list(pathlib.Path(__file__, "..", "..", "examples").resolve().glob("*
 
 @pytest.mark.parametrize("example", examples, ids=lambda example: example.stem)
 def test_example(example):
-    runpy.run_path(example)
+    try:
+        runpy.run_path(example)
+    except SystemExit:  # allow for early termination of scripts
+        pass
