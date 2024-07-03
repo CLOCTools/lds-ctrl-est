8db0a88d17c74db7cf9f6e139755e66ab938db15
Kyle Johnsen <johnsenkyle13@gmail.com>
2024-07-03 11:55:07 -0400

    automate OSQP build

--- cmake/Modules/OSQP.cmake
+++ cmake/Modules/OSQP.cmake
@@ -1,2 +1,13 @@
+include(FetchContent)
+message(STATUS "Fetching & installing osqp")
+FetchContent_Declare(
+    osqp
+    PREFIX ${CMAKE_BINARY_DIR}/osqp
+    GIT_REPOSITORY https://github.com/osqp/osqp.git
+    GIT_TAG 4e81a9d0
+)
+FetchContent_MakeAvailable(osqp)
+message(STATUS "Installed osqp to ${osqp_BINARY_DIR}")
+list(APPEND CMAKE_PREFIX_PATH ${osqp_BINARY_DIR})
 find_package(osqp REQUIRED)
 list(APPEND PROJECT_REQUIRED_LIBRARIES_ABSOLUTE_NAME osqp::osqp)
