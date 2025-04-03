022e03b9b807f87802cf14b93a033d3eb9e4ef5b
Muhammad Yousuf <muhammad.y.2305@gmail.com>
2025-04-03 13:49:22 -0400

    build docs updated, not deleted

--- scripts/update-docs.sh
+++ scripts/update-docs.sh
@@ -9,5 +9,5 @@ cd ../misc/docs-hugo && hugo && cd ../../scripts
 # create docs directory if it doesn't exist
 mkdir -p ../docs
 
-# update docs by copying new files, preserving existing ones that haven't changed
-cp -ru ../misc/docs-hugo/public/* ../docs/
+# update docs by copying new files and overwriting changed ones
+cp -R ../misc/docs-hugo/public/* ../docs/
